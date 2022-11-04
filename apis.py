95f52833d2f6841b5ec6a9885f84f5bc65dff10b
Lander-Hatsune <wangwwx@yeah.net>
2022-11-04 15:50:17 +0800

    apis: getChannelAbout done

--- apis.py
+++ apis.py
@@ -1,20 +1,47 @@
 import re
 import json
+import urllib
 import requests
 
+headers = {'Accept-Language': 'en-US'}
 cookies = {'CONSENT': 'YES+'}
 
 def getChannelAbout(channel_id: str):
     url = f'https://www.youtube.com/channel/{channel_id}/about'
     pattern = re.compile(r'var ytInitialData = (.*?);</script>')
-    resp = requests.get(url, cookies=cookies)
+    resp = requests.get(url, headers=headers, cookies=cookies)
     json_text = pattern.search(resp.text).group(1)
     data = json.loads(json_text)
-    about = next(tab for tab in data['contents']['tabs'] if tab.get('selected')) \
-        ['content']['channelAboutFullMetadataRenderer']
-        
+
+    about = next(
+        tab['tabRenderer']['content'] for tab in \
+        data['contents']['twoColumnBrowseResultsRenderer']['tabs'] \
+        if tab['tabRenderer'].get('selected'))
+
+    about = about['sectionListRenderer']['contents'][0] \
+        ['itemSectionRenderer']['contents'][0] \
+        ['channelAboutFullMetadataRenderer']
+
+    link_pattern = re.compile(r'&q=(.*)')
+    ret = {
+        'title': about['title']['simpleText'],
+        'description': about['description']['simpleText'],
+        'avatar': about['avatar']['thumbnails'][-1]['url'],
+        'country': about['country']['simpleText'],
+        'viewCount': about['viewCountText']['simpleText'],
+        'joinedDate': about['joinedDateText']['runs'][1]['text'],
+        'links': [{
+            'title': link['title']['simpleText'],
+            'link': urllib.parse.unquote(link_pattern.search(
+                link['navigationEndpoint']['urlEndpoint']['url']).group(1)),
+        } for link in about['primaryLinks']],
+        'channelId': about['channelId'],
+    }
+    assert(ret['channelId'] == channel_id)
+    return ret
         
 session = requests.session() # for https://www.googleapis.com/youtube/v3/
 
 if __name__ == '__main__':
-    getChannelAbout('UCf5CA0OsvhhU-6AcSjT1oKQ')
+    channelAbout = getChannelAbout('UCf5CA0OsvhhU-6AcSjT1oKQ')
+    print(channelAbout)
